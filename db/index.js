0a4ac80d79d4dfab28bebf095ea7f741158b8be1
theBowja <ericxin@gmail.com>
2020-08-25 22:49:28 -0500

    added pg as package

--- db/index.js
+++ db/index.js
@@ -1,12 +1,20 @@
+const { Pool } = require('pg');
+const pool = new Pool();
+
+
+
+
 let mydb = {};
 
 
 
-
-
-
-
-
+// redo this programmatically probably
+mydb.channels = require('./modelpg/channels.js');
+mydb.currentsnap = require('./modelpg/currentsnap.js');
+mydb.discokids = require('./modelpg/discokids.js');
+mydb.metareqs = require('./modelpg/metareqs.js');
+mydb.requirements = require('./modelpg/requirements.js');
+mydb.version = require('./modelpg/version.js');
 
 
 
