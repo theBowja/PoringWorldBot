3502c4c44c713ce2b3638391ecad3ead8d20c6dc
theBowja <ericxin@gmail.com>
2022-09-06 11:48:12 -0400

    application command

--- deploy-commands.js
+++ deploy-commands.js
@@ -20,11 +20,13 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+// 	.then(() => console.log('Successfully registered application commands.'))
+// 	.catch(console.error);
+
+rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
-
-
+ 	.catch(console.error);
 
 // DELETING COMMANDS
 // // for global commands
